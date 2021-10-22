31d596b964e4eb182397b97509ddf57e967e4a68
Khuzaima-bashir <khuzaima.bashir@arbisoft.com>
2021-10-22 16:21:23 +0500

    url correction

--- arbistore/urls.py
+++ arbistore/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
                   path('categories/', Categories.as_view(), name='categories'),
                   path('register/', RegisterView.as_view(), name='register'),
                   path('category/<int:pk>/', CategoryProductFetch.as_view(), name='category_products_fetch'),
-                  path('sub_category/<int:pk>/', SubCategoryProductFetch.as_view(), name='sub_category_products_fetch'),
+                  path('subcategory/<int:pk>/', SubCategoryProductFetch.as_view(), name='sub_category_products_fetch'),
                   path('product_view/<int:pk>/', ProductFetch.as_view(), name='product_fetch'),
                   path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                   path('api-auth/', include('rest_framework.urls')),
